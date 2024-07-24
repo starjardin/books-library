94bfc46dbe10d8c4a1446c56c7531b990e7570eb
starjardin <tantely.and@onja.org>
2024-07-24 08:55:36 +0300

    [feat] Connect application to database

---
+++ backend/db/tantely.go
@@ -0,0 +1,13 @@
+// Forget about the code below, it's just a test
+package database
+
+import "fmt"
+
+func GetName() string {
+	return "Tantely"
+}
+
+func main() {
+	name := GetName()
+	fmt.Println(name)
+}
